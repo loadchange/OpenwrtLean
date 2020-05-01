c51a5623c22f9b9eec31d0cfa65f6352250558fa
LoadChange <soocto@gmail.com>
2020-05-02 01:23:18 +0800

    add file themes/baiyutang/footer.php

--- themes/baiyutang/footer.php
+++ themes/baiyutang/footer.php
@@ -1,18 +1,15 @@
-			<!-- footer -->
-			<footer class="footer" role="contentinfo">
-
-				<!-- copyright -->
-				<p class="copyright">
-					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?>
-					<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
-				</p>
-				<!-- /copyright -->
-
-			</footer>
-			<!-- /footer -->
-
-		</div>
-		<!-- /wrapper -->
-		<?php wp_footer(); ?>
-	</body>
+<footer>
+  <div class="container">
+    <?php
+    wp_nav_menu(
+      array(
+        'theme_location' => 'footer-menu',
+        'menu_class' => 'footer-bar'
+      )
+    )
+    ?>
+  </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
 </html>
