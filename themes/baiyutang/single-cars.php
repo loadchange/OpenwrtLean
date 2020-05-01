0b2f59b3f3f5909bf18b9042320f0aa2aee7d503
LoadChange <soocto@gmail.com>
2020-05-02 01:23:18 +0800

    add file themes/baiyutang/single-cars.php

--- themes/baiyutang/single-cars.php
+++ themes/baiyutang/single-cars.php
@@ -2,34 +2,25 @@
 
 <section class="page-wrap">
   <div class="container">
+    <h1><?php the_title() ?></h1>
+    <?php if (has_post_thumbnail()): ?>
+      <img src="<?php the_post_thumbnail_url('large'); ?>" alt="<?php the_title(); ?>"
+           class="img-fluid mb-3 img-thumbnail"/>
+    <?php endif; ?>
+
+<div class="row">
+  <div class="col-lg-6">
+
+    <?php get_template_part('includes/section', 'cars'); ?>
 
 
-    <div class="row">
-      <div class="col-lg-9">
-        <?php if (has_post_thumbnail()): ?>
-          <img src="<?php the_post_thumbnail_url('large'); ?>" alt="<?php the_title(); ?>"
-               class="img-fluid mb-3 img-thumbnail"/>
-        <?php endif; ?>
-        <h1><?php the_title() ?></h1>
-        <?php get_template_part('includes/section', 'blogcontent'); ?>
+    <?php wp_link_pages(); ?>
+  </div>
+</div>
 
-        <?php
-        // comments_template();
-        ?>
 
-        <?php wp_link_pages(); ?>
-      </div>
-      <div class="col-lg-3">
 
-        <?php if (is_active_sidebar('page-sidebar')): ?>
 
-          <ul class="page-widget">
-            <?php dynamic_sidebar('page-sidebar') ?>
-          </ul>
-
-        <?php endif; ?>
-
-      </div>
 
     </div>
 
