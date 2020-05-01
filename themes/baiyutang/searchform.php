88b3122c77b55c212b93e356aa1f689005e88514
LoadChange <soocto@gmail.com>
2020-05-02 01:23:18 +0800

    add file themes/baiyutang/searchform.php

--- themes/baiyutang/searchform.php
+++ themes/baiyutang/searchform.php
@@ -1,1 +1,5 @@
-<?php
+<form action="/" method="get">
+  <label for="search">Search</label>
+  <input name="s" id="search" value="<?php the_search_query(); ?>" required/>
+  <button type="submit">Search!</button>
+</form>
