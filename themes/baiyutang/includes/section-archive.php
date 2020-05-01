788ea5598a2a5c1bc1e0306d8f2603825c62aa58
LoadChange <soocto@gmail.com>
2020-05-02 01:23:18 +0800

    add file themes/baiyutang/includes/section-archive.php

--- themes/baiyutang/includes/section-archive.php
+++ themes/baiyutang/includes/section-archive.php
@@ -1,10 +1,19 @@
-<?php get_header(); ?>
-
-<section class="page-wrap">
-  <div class="container">
-    <h1><?php the_title() ?></h1>
-    <?php get_template_part('includes/section', 'content'); ?>
+<?php if (have_posts()):while (have_posts()):the_post(); ?>
+  <div class="card mb-3">
+    <div class="card-body d-flex align-items-center">
+      <?php if (has_post_thumbnail()): ?>
+      <div class="cat-img-warp img-thumbnail">
+        <img src="<?php the_post_thumbnail_url('medium'); ?>" alt="<?php the_title(); ?>"
+             class="img-fluid mb-3 cat-img"/>
+      </div>
+      <?php endif; ?>
+      <div class="blog-content">
+        <h3><?php the_title() ?></h3>
+        <?php the_excerpt(); ?>
+        <a href="<?php the_permalink(); ?>" class="btn btn-success">Read more</a>
+      </div>
+    </div>
   </div>
-</section>
 
-<?php get_footer(); ?>
+  <hr/>
+<?php endwhile; endif; ?>
