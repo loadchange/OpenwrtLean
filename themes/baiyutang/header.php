af0b2719d9cbf2054f086d377555aecbbc913a6f
LoadChange <soocto@gmail.com>
2020-05-02 01:23:18 +0800

    add file themes/baiyutang/header.php

--- themes/baiyutang/header.php
+++ themes/baiyutang/header.php
@@ -1,49 +1,23 @@
 <!doctype html>
-<html <?php language_attributes(); ?> class="no-js">
+<html lang="en">
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <title><?php wp_title(''); ?><?php if (wp_title('', false)) {
-            echo ' :';
-        } ?><?php bloginfo('name'); ?></title>
-    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
-    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="<?php bloginfo('description'); ?>">
-    <?php wp_head(); ?>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <?php wp_head(); ?>
 </head>
-<body <?php body_class(); ?>>
-
-<!-- wrapper -->
-<div class="wrapper">
-    <!-- header -->
-    <header class="header" role="banner">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <!-- logo -->
-            <a class="navbar-brand" href="<?php echo home_url(); ?>">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" width="30" height="30"
-                     class="d-inline-block align-top" alt="">
-            </a>
-            <!-- /logo -->
-
-            <!-- nav -->
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
-                    aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                </ul>
-                <?php html5blank_nav(); ?>
-            </div>
-            <form class="form-inline">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-            <!-- /nav -->
-        </nav>
-    </header>
-    <!-- /header -->
+<body>
+<header>
+  <div class="container">
+    <?php
+    wp_nav_menu(
+      array(
+        'theme_location' => 'top-menu',
+        'menu_class' => 'top-bar'
+      )
+    )
+    ?>
+  </div>
+</header>
