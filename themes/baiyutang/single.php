52545567d03da6014be558687fbcb7357acb71d0
LoadChange <soocto@gmail.com>
2020-05-02 01:23:18 +0800

    add file themes/baiyutang/single.php

--- themes/baiyutang/single.php
+++ themes/baiyutang/single.php
@@ -1,67 +1,41 @@
 <?php get_header(); ?>
 
-	<main role="main">
-	<!-- section -->
-	<section>
+<section class="page-wrap">
+  <div class="container">
 
-	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <div class="row">
+      <div class="col-lg-9">
+        <?php if (has_post_thumbnail()): ?>
+          <img src="<?php the_post_thumbnail_url('large'); ?>" alt="<?php the_title(); ?>"
+               class="img-fluid mb-3 img-thumbnail"/>
+        <?php endif; ?>
+        <h1><?php the_title() ?></h1>
+        <?php get_template_part('includes/section', 'blogcontent'); ?>
 
-			<!-- post thumbnail -->
-			<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-					<?php the_post_thumbnail(); // Fullsize image for the single post ?>
-				</a>
-			<?php endif; ?>
-			<!-- /post thumbnail -->
+        <?php
+        // comments_template();
+        ?>
 
-			<!-- post title -->
-			<h1>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-			</h1>
-			<!-- /post title -->
+        <?php wp_link_pages(); ?>
+      </div>
+      <div class="col-lg-3">
 
-			<!-- post details -->
-			<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-			<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-			<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-			<!-- /post details -->
+        <?php if (is_active_sidebar('page-sidebar')): ?>
 
-			<?php the_content(); // Dynamic Content ?>
+          <ul class="page-widget">
+            <?php dynamic_sidebar('page-sidebar') ?>
+          </ul>
 
-			<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
+        <?php endif; ?>
 
-			<p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
+      </div>
 
-			<p><?php _e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
+    </div>
 
-			<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
 
-			<?php comments_template(); ?>
-
-		</article>
-		<!-- /article -->
-
-	<?php endwhile; ?>
-
-	<?php else: ?>
-
-		<!-- article -->
-		<article>
-
-			<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
-
-		</article>
-		<!-- /article -->
-
-	<?php endif; ?>
-
-	</section>
-	<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+  </div>
+</section>
 
 <?php get_footer(); ?>
+
