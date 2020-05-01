37c916646e8634cd0f7533ed19be123571a86305
LoadChange <soocto@gmail.com>
2020-05-02 01:23:18 +0800

    add file themes/baiyutang/includes/section-searchresults.php

--- themes/baiyutang/includes/section-searchresults.php
+++ themes/baiyutang/includes/section-searchresults.php
@@ -2,10 +2,10 @@
   <div class="card mb-3">
     <div class="card-body d-flex align-items-center">
       <?php if (has_post_thumbnail()): ?>
-      <div class="cat-img-warp img-thumbnail">
-        <img src="<?php the_post_thumbnail_url('medium'); ?>" alt="<?php the_title(); ?>"
-             class="img-fluid mb-3 cat-img"/>
-      </div>
+        <div class="cat-img-warp img-thumbnail">
+          <img src="<?php the_post_thumbnail_url('medium'); ?>" alt="<?php the_title(); ?>"
+               class="img-fluid mb-3 cat-img"/>
+        </div>
       <?php endif; ?>
       <div class="blog-content">
         <h3><?php the_title() ?></h3>
@@ -16,4 +16,8 @@
   </div>
 
   <hr/>
-<?php endwhile; endif; ?>
+<?php endwhile; else: ?>
+
+  There are no results for '<?php echo get_search_query();?>'
+
+<?php endif; ?>
